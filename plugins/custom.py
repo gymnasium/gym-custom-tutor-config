781738804e3047c706c343607bf56e3c7202d352
Roman Edirisinghe <rediris@users.noreply.github.com>
2024-02-07 10:18:09 -0500

    Update custom.py

--- plugins/custom.py
+++ plugins/custom.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-response = requests.get('http://local.overhang.io/feeds/config.json')
+response = requests.get('http://local.edly.io/feeds/config.json')
 if response.status_code == 200:
     data = response.json()
 else:
@@ -24,51 +24,51 @@ def _remove_stock_mfes(mfes):
 
 # Add custom MFEs
 def mfe_forks(mfes):
-    mfes["account"] = {
-        "repository": "https://github.com/gymnasium/frontend-app-account",
-        "version": "gym.palm.4",
-        "refs": "https://api.github.com/repos/gymnasium/frontend-app-account/git/refs/heads",
-        "port": 1997,
-        "name": "account",
-    }
-    mfes["authn"] = {
-        "repository": "https://github.com/gymnasium/frontend-app-authn",
-        "version": "gym.palm.4",
-        "refs": "https://api.github.com/repos/gymnasium/frontend-app-authn/git/refs/heads",
-        "port": 1999,
-        "name": "authn",
-    }
+    # mfes["account"] = {
+    #     "repository": "https://github.com/gymnasium/frontend-app-account",
+    #     "version": "gym.quince.1",
+    #     "refs": "https://api.github.com/repos/gymnasium/frontend-app-account/git/refs/heads",
+    #     "port": 1997,
+    #     "name": "account",
+    # }
+    # mfes["authn"] = {
+    #     "repository": "https://github.com/gymnasium/frontend-app-authn",
+    #     "version": "gym.quince.1",
+    #     "refs": "https://api.github.com/repos/gymnasium/frontend-app-authn/git/refs/heads",
+    #     "port": 1999,
+    #     "name": "authn",
+    # }
     # mfes["communications"] = {
     #     "repository": "https://github.com/gymnasium/frontend-app-communications",
-    #     "version": "gym.palm.4",
+    #     "version": "gym.quince.1",
     #     "refs": "https://api.github.com/repos/gymnasium/frontend-app-communications/git/refs/heads",
     #     "port": 1984,
     #     "name": "communications",
     # }
-    mfes["course-about"] = {
-        "repository": "https://github.com/gymnasium/frontend-app-course-about",
-        "version": "gym.palm.4",
-        "refs": "https://api.github.com/repos/gymnasium/frontend-app-course-about/git/refs/heads",
-        "port": 3000,
-        "name": "course-about",
-    }
+    # mfes["course-about"] = {
+    #     "repository": "https://github.com/gymnasium/frontend-app-course-about",
+    #     "version": "gym.quince.1",
+    #     "refs": "https://api.github.com/repos/gymnasium/frontend-app-course-about/git/refs/heads",
+    #     "port": 3000,
+    #     "name": "course-about",
+    # }
     # mfes["course-authoring"] = {
     #     "repository": "https://github.com/gymnasium/frontend-app-course-authoring",
-    #     "version": "gym.palm.4",
+    #     "version": "gym.quince.1",
     #     "refs": "https://api.github.com/repos/gymnasium/frontend-app-course-authoring/git/refs/heads",
     #     "port": 2001,
     #     "name": "course_authoring",
     # }
-    mfes["discussions"] = {
-        "repository": "https://github.com/gymnasium/frontend-app-discussions",
-        "version": "gym.palm.4",
-        "refs": "https://api.github.com/repos/gymnasium/frontend-app-discussions/git/refs/heads",
-        "port": 2002,
-        "name": "discussions",
-    }
+    # mfes["discussions"] = {
+    #     "repository": "https://github.com/gymnasium/frontend-app-discussions",
+    #     "version": "gym.quince.1",
+    #     "refs": "https://api.github.com/repos/gymnasium/frontend-app-discussions/git/refs/heads",
+    #     "port": 2002,
+    #     "name": "discussions",
+    # }
     # mfes["gradebook"] = {
     #     "repository": "https://github.com/gymnasium/frontend-app-gradebook",
-    #     "version": "gym.palm.4",
+    #     "version": "gym.quince.1",
     #     "refs": "https://api.github.com/repos/gymnasium/frontend-app-gradebook/git/refs/heads",
     #     "port": 1994,
     #     "name": "gradebook",
@@ -80,33 +80,33 @@ def mfe_forks(mfes):
     #     "port": 3001,
     #     "name": "home",
     # }
-    mfes["learner-dashboard"] = {
-        "repository": "https://github.com/gymnasium/frontend-app-learner-dashboard",
-        "version": "gym.palm.4",
-        "refs": "https://api.github.com/repos/gymnasium/frontend-app-learner-dashboard/git/refs/heads",
-        "port": 1996,
-    }
-    mfes["learning"] = {
-        "repository": "https://github.com/gymnasium/frontend-app-learning",
-        "version": "gym.palm.4",
-        "refs": "https://api.github.com/repos/gymnasium/frontend-app-learning/git/refs/heads",
-        "port": 2000,
-        "name": "learning",
-    }
+    # mfes["learner-dashboard"] = {
+    #     "repository": "https://github.com/gymnasium/frontend-app-learner-dashboard",
+    #     "version": "gym.quince.1",
+    #     "refs": "https://api.github.com/repos/gymnasium/frontend-app-learner-dashboard/git/refs/heads",
+    #     "port": 1996,
+    # }
+    # mfes["learning"] = {
+    #     "repository": "https://github.com/gymnasium/frontend-app-learning",
+    #     "version": "gym.quince.1",
+    #     "refs": "https://api.github.com/repos/gymnasium/frontend-app-learning/git/refs/heads",
+    #     "port": 2000,
+    #     "name": "learning",
+    # }
     # mfes["ora-grading"] = {
     #     "repository": "https://github.com/gymnasium/frontend-app-ora-grading",
-    #     "version": "gym.palm.4",
+    #     "version": "gym.quince.1",
     #     "refs": "https://api.github.com/repos/gymnasium/frontend-app-ora-grading/git/refs/heads",
     #     "port": 1993,
     #     "name": "ora-grading",
     # }
-    mfes["profile"] = {
-        "repository": "https://github.com/gymnasium/frontend-app-profile",
-        "version": "gym.palm.4",
-        "refs": "https://api.github.com/repos/gymnasium/frontend-app-profile/git/refs/heads",
-        "port": 1995,
-        "name": "profile",
-    }
+    # mfes["profile"] = {
+    #     "repository": "https://github.com/gymnasium/frontend-app-profile",
+    #     "version": "gym.quince.1",
+    #     "refs": "https://api.github.com/repos/gymnasium/frontend-app-profile/git/refs/heads",
+    #     "port": 1995,
+    #     "name": "profile",
+    # }
     
     return mfes
 
@@ -121,8 +121,8 @@ CORS_ORIGIN_WHITELIST.append("{data["urls"]["root"]}")
 CSRF_TRUSTED_ORIGINS.append("{data["urls"]["root"]}")
 LOGIN_REDIRECT_WHITELIST.append("{data["urls"]["root"]}")
 
-SESSION_COOKIE_DOMAIN="overhang.io"
-SHARED_COOKIE_DOMAIN = "overhang.io"
+SESSION_COOKIE_DOMAIN="edly.io"
+SHARED_COOKIE_DOMAIN = "edly.io"
 
 """,
 ),
@@ -150,14 +150,13 @@ MFE_CONFIG["MARKETING_SITE_ROOT"] = '{data["urls"]["root"]}'
 (
 "mfe-dockerfile-post-npm-install",
 """
-RUN echo "working dir"; PWD;
-
-ADD https://api.github.com/repos/gymnasium/brand-openedx/git/refs/heads/gym.palm.4 /tmp/gitref-brand
-
-COPY "$TUTOR_ROOT/mfe/brand-openedx /openedx/brand-openedx"
+ADD https://api.github.com/repos/gymnasium/brand-openedx/git/refs/heads/gym.quince.1 /tmp/gitref-brand
 RUN npm install '@openedx/brand-openedx@file:../brand-openedx' --registry=$NPM_REGISTRY
 
-COPY "$TUTOR_ROOT/mfe/gym-frontend-components /openedx/app/src/gym-frontend-components"
+# ADD https://api.github.com/repos/gymnasium/gym-frontend-components/git/refs/heads/gym.quince.1 /tmp/gitref-components
+
+RUN git clone --depth 1 -b gym.quince.1 https://github.com/gymnasium/gym-frontend-components.git /openedx/app/src/gym-frontend-components
+
 """,
 ),
 ],
