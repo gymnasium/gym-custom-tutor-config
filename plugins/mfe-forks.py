30f2ddff168339dd8529b2d3ba2c1467e0ebf5c7
Roman Edirisinghe <rediris@users.noreply.github.com>
2024-04-22 13:51:43 -0400

    Update mfe-forks.py

--- plugins/mfe-forks.py
+++ plugins/mfe-forks.py
@@ -24,13 +24,13 @@ def mfe_forks(mfes):
         "port": 3000,
         "name": "course-about",
     }
-    # mfes["discussions"] = {
-    #     "repository": "https://github.com/gymnasium/frontend-app-discussions.git",
-    #     "version": "gym.quince.1",
-    #     "refs": "https://api.github.com/repos/gymnasium/frontend-app-discussions/git/refs/heads",
-    #     "port": 2002,
-    #     "name": "discussions",
-    # }
+    mfes["discussions"] = {
+        "repository": "https://github.com/gymnasium/frontend-app-discussions.git",
+        "version": "gym.quince.1",
+        "refs": "https://api.github.com/repos/gymnasium/frontend-app-discussions/git/refs/heads",
+        "port": 2002,
+        "name": "discussions",
+    }
     mfes["learner-dashboard"] = {
         "repository": "https://github.com/gymnasium/frontend-app-learner-dashboard.git",
         "version": "gym.quince.3",
