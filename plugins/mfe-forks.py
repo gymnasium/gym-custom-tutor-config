fc637c3eb5d26dd192d8818c8cf80d79c6e612d8
Roman Edirisinghe <rediris@users.noreply.github.com>
2024-07-24 13:06:02 -0400

    Update mfe-forks.py

--- plugins/mfe-forks.py
+++ plugins/mfe-forks.py
@@ -26,7 +26,7 @@ def mfe_forks(mfes):
     }
     mfes["discussions"] = {
         "repository": "https://github.com/gymnasium/frontend-app-discussions.git",
-        "version": "gym.quince.3",
+        "version": "gym.redwood",
         "refs": "https://api.github.com/repos/gymnasium/frontend-app-discussions/git/refs/heads",
         "port": 2002,
         "name": "discussions",
@@ -47,7 +47,7 @@ def mfe_forks(mfes):
     }
     mfes["profile"] = {
         "repository": "https://github.com/gymnasium/frontend-app-profile.git",
-        "version": "gym.quince.3",
+        "version": "gym.redwood",
         "refs": "https://api.github.com/repos/gymnasium/frontend-app-profile/git/refs/heads",
         "port": 1995,
         "name": "profile",
